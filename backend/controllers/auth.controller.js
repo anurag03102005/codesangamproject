ac6a4448b9e11006f9dcdd9469c3f37b79bc96e5
anurag03102005 <anurag03102005@gmail.com>
2024-11-07 13:26:14 +0530

    login and signup functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,32 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
-function login(req,res){
+async function login(req,res){
+    const username= req.body.username;
+    const password = req.body.password;
+    const existingUser = await User.findOne({ username: username });
+    if(!existingUser){
+        return res.status(400).json({ message: 'Username already exist' });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:'Incorrect password'});
+    }
     console.log("login");
 }
 async function signUp(req,res){
     const username= req.body.username;
     const password = req.body.password;
+    const confirm = req.body.confirm;
+    const existingUser = await User.findOne({ username: username });
+    if(existingUser){
+        return res.status(400).json({ message: 'Username already exist' });
+    }
+    if(confirm!==password){
+        return res.status(400).json({ message: 'Passwords do not match' });
+    }
+    if(password.length<=5){
+        return res.send(400).json({message:'password length should be more than 5 characters'})
+    }
     const salt = await bcrypt.genSalt(10);
 	const hashedPassword = await bcrypt.hash(password, salt);
     const newuser = new User({username,password:hashedPassword});
